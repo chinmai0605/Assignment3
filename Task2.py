5f557d68e7afb08ec646884a5b14b8667bb9228a
chinmai0605 <chinmai0605@gmail.com>
2026-03-25 19:48:02 +0530

    Initial commit

---
+++ Task2.py
@@ -0,0 +1,11 @@
+import math
+
+num = float(input("Enter a number: "))
+
+sqrt_val = math.sqrt(num)        
+log_val = math.log(num)          
+sin_val = math.sin(num)          
+
+print("Square root:", sqrt_val)
+print("Logarithm:", log_val)
+print("Sine:", sin_val)
